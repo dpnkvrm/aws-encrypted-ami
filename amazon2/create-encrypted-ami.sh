24860c81a2b49561019800ce7ae795aa3458d5b2
Deepank Verma <deepank@alphafortress.com>
2020-06-09 20:53:32 +0530

    renamed file

--- amazon2/create-encrypted-ami.sh
+++ amazon2/create-encrypted-ami.sh
@@ -6,8 +6,6 @@ read awsaccount
 echo "region:"
 read region
 eval "$(faws -r $rackaccount env -a $awsaccount)"
-# eval "$(faws -r 981064 env -a 805405598173)"
-# region="eu-central-1"
 terraform plan -var "region=$region" --out tf.plan 
 terraform apply tf.plan 
 ami_id=`cat terraform.tfstate | jq .modules[].outputs.encrypted_amiid.value | tr -d '"'`
